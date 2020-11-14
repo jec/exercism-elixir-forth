27667189633a67f856cd8760de6e26be7f570562
Jim Cain <jim@jcain.net>
2020-11-14 03:17:28 -0500

    Predefine stack ops in the default dict
    Define eval_stackop/1 to handle :stackop tokens
    Consolidate startdef==true case for eval_token/2

--- lib/forth.ex
+++ lib/forth.ex
@@ -3,7 +3,14 @@ defmodule Forth do
   Implements a simple Forth evaluator
   """
 
-  defstruct [stack: [], dict: %{}, startdef: false]
+  @default_dict %{
+    "dup" => [stackop: :dup],
+    "drop" => [stackop: :drop],
+    "swap" => [stackop: :swap],
+    "over" => [stackop: :over]
+  }
+
+  defstruct [stack: [], dict: @default_dict, startdef: false]
 
   @type t :: %Forth{stack: stack(), dict: %{}, startdef: boolean}
 
@@ -12,6 +19,8 @@ defmodule Forth do
   @type stack :: [integer | t()]
 
   @type binary_op :: ?+ | ?- | ?* | ?/
+
+  @type stack_op :: :dup | :drop | :swap | :over
 
   @doc """
   Create a new evaluator.
@@ -46,42 +55,86 @@ defmodule Forth do
   # Evaluates a token in the context of a Forth struct and returns an updated
   # Forth struct
   @spec eval_token({Forth.Tokenizer.token_type(), any}, t()) :: t()
-
-  # int
-  defp eval_token(token = {:int, _}, forth = %Forth{startdef: true, stack: [command_def | stack]}) do
-    %Forth{forth | stack: [[token | command_def] | stack]}
-  end
   defp eval_token({:int, int}, forth = %Forth{startdef: false, stack: stack}) do
     %Forth{forth | stack: [int | stack]}
-  end
-
-  # op
-  defp eval_token(token = {:op, _}, forth = %Forth{startdef: true, stack: [command_def | stack]}) do
-    %Forth{forth | stack: [[token | command_def] | stack]}
   end
   defp eval_token({:op, op}, forth = %Forth{startdef: false, stack: stack}) do
     %Forth{forth | stack: eval_operator(op, stack)}
   end
-
-  # id
-  defp eval_token(token = {:id, _}, forth = %Forth{startdef: true, stack: [command_def | stack]}) do
-    %Forth{forth | stack: [[token | command_def] | stack]}
-  end
   defp eval_token({:id, command}, forth = %Forth{startdef: false}) do
     String.downcase(command) |> execute_command(forth)
   end
-
-  # startdef
+  defp eval_token({:stackop, op}, forth = %Forth{startdef: false, stack: stack}) do
+    %Forth{forth | stack: eval_stackop(op, stack)}
+  end
   defp eval_token({:startdef, _}, forth = %Forth{startdef: false, stack: stack}) do
+    # Push an empty list onto the stack, which we will use to collect the
+    # tokens until we encounter an :enddef.
     %Forth{forth | startdef: true, stack: [[] | stack]}
   end
-
-  # enddef
   defp eval_token({:enddef, _}, %Forth{startdef: true, stack: [command_def | stack], dict: dict}) do
+    # Reverse the list that contains the tokens. Now the first token is the
+    # name of the command to be defined, and the remaining tokens are the
+    # definition.
     case Enum.reverse(command_def) do
       [{:id, name} | tokens] ->
         %Forth{startdef: false, stack: stack, dict: Map.put(dict, name, tokens)}
       [{:int, int} | _] -> raise Forth.InvalidWord, word: int
+    end
+  end
+  # any other token when startdef is true
+  defp eval_token(token, forth = %Forth{startdef: true, stack: [command_def | stack]}) do
+    %Forth{forth | stack: [[token | command_def] | stack]}
+  end
+
+  # Evaluates a binary operator in the context of a stack
+  @spec eval_operator(binary_op(), stack()) :: stack()
+  defp eval_operator(?+, [y | [x | rest]]) do
+    [x + y | rest]
+  end
+  defp eval_operator(?-, [y | [x | rest]]) do
+    [x - y | rest]
+  end
+  defp eval_operator(?*, [y | [x | rest]]) do
+    [x * y | rest]
+  end
+  defp eval_operator(?/, [0 | _]) do
+    raise Forth.DivisionByZero
+  end
+  defp eval_operator(?/, [y | [x | rest]]) do
+    [div(x, y) | rest]
+  end
+
+  # Evaluates a built-in stack operator in the context of a stack
+  @spec eval_stackop(stack_op(), stack()) :: stack()
+  defp eval_stackop(_, []) do
+    raise Forth.StackUnderflow
+  end
+  defp eval_stackop(:dup, [head | tail]) do
+    [head | [head | tail]]
+  end
+  defp eval_stackop(:drop, [_ | tail]) do
+    tail
+  end
+  defp eval_stackop(:swap, [_]) do
+    raise Forth.StackUnderflow
+  end
+  defp eval_stackop(:swap, [y | [x | rest]]) do
+    [x | [y | rest]]
+  end
+  defp eval_stackop(:over, [_]) do
+    raise Forth.StackUnderflow
+  end
+  defp eval_stackop(:over, stack = [_ | [x | _]]) do
+    [x | stack]
+  end
+
+  # Executes a named command
+  @spec execute_command(String.t(), t()) :: t()
+  defp execute_command(command, forth) do
+    case forth.dict do
+      %{^command => tokens} -> eval_tokens(forth, tokens)
+      _ -> raise Forth.UnknownWord, word: command
     end
   end
 
@@ -93,78 +146,6 @@ defmodule Forth do
   defp eval_tokens(forth, [token | rest]) do
     next_forth = eval_token(token, forth)
     eval_tokens(next_forth, rest)
-  end
-
-  # Evaluates a binary operator in the context of a stack
-  @spec eval_operator(binary_op(), stack()) :: stack()
-
-  # addition
-  defp eval_operator(?+, [y | [x | rest]]) do
-    [x + y | rest]
-  end
-
-  # subtraction
-  defp eval_operator(?-, [y | [x | rest]]) do
-    [x - y | rest]
-  end
-
-  # multiplication
-  defp eval_operator(?*, [y | [x | rest]]) do
-    [x * y | rest]
-  end
-
-  # integer division
-  defp eval_operator(?/, [0 | _]) do
-    raise Forth.DivisionByZero
-  end
-  defp eval_operator(?/, [y | [x | rest]]) do
-    [div(x, y) | rest]
-  end
-
-  # Executes a named command
-  @spec execute_command(String.t(), t()) :: t()
-
-  defp execute_command("dup", %Forth{stack: []}) do
-    raise Forth.StackUnderflow
-  end
-  defp execute_command("dup", forth) do
-    %Forth{forth | stack: [hd(forth.stack) | forth.stack]}
-  end
-
-  defp execute_command("drop", %Forth{stack: []}) do
-    raise Forth.StackUnderflow
-  end
-  defp execute_command("drop", forth) do
-    %Forth{forth | stack: tl(forth.stack)}
-  end
-
-  defp execute_command("swap", %Forth{stack: []}) do
-    raise Forth.StackUnderflow
-  end
-  defp execute_command("swap", %Forth{stack: [_]}) do
-    raise Forth.StackUnderflow
-  end
-  defp execute_command("swap", forth) do
-    [y | [x | rest]] = forth.stack
-    %Forth{forth | stack: [x | [y | rest]]}
-  end
-
-  defp execute_command("over", %Forth{stack: []}) do
-    raise Forth.StackUnderflow
-  end
-  defp execute_command("over", %Forth{stack: [_]}) do
-    raise Forth.StackUnderflow
-  end
-  defp execute_command("over", forth) do
-    [_ | [x | _]] = forth.stack
-    %Forth{forth | stack: [x | forth.stack]}
-  end
-
-  defp execute_command(command, forth) do
-    case forth.dict do
-      %{^command => tokens} -> eval_tokens(forth, tokens)
-      _ -> raise Forth.UnknownWord, word: command
-    end
   end
 
   defmodule StackUnderflow do
